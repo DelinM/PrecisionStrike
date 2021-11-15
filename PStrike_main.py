f2dd0279fb39a9aa499b7d1bf7cc4234a74d0fa1
DelinM <delinmuray@gmail.com>
2021-11-14 22:32:08 -0500

    update

--- PStrike_main.py
+++ PStrike_main.py
@@ -18,8 +18,7 @@ class PongBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
 
-    velocity = ReferenceListProperty(velocity_x,
-                                     velocity_y)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
